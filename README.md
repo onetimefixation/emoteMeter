68a20a02a6e595d252df6b80951a991665134f27
astro <onetimefixation@gmail.com>
2021-12-18 11:40:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Emote Meter
+Used by viewers to spam emotes in chat
+When the chat reaches a certain threshold, this will trigger a "!boom" command
+This command spams a predefined overlay with emotes
+
+The emote configuration is controled by the xxxxxxx feature of Stream Elements
+
+
+
+## METHOD 1
+
+
+
+
+## Getting Started
+
+Follow the instructions below to install these additional commands to your Twitch channel
+
+### Prerequisites
+
+
+
+
+### Installing to Stream Elements and OBS
+
+https://www.youtube.com/watch?v=XWwUMxYgbtE
+
+Stream Elements
+    Chat Commands
+        kappagen / boom
+    My Overlays
+        new
+        Position, size and style
+            ?????
+            Center Widget
+            SAVE
+        EMOTESPLOSIONS
+        SELECT EMOTES
+            SAVE
+
+    Copy URL (on top right (chain))
+
+
+OBS
+    Create New Source - BROWSER
+    Name it
+    Paste the copied URL
+    Change WIDTH and HEIGHT to ???????????????
+
+
+
+
+## Built With
+
+index.html
+styles.css
+emote_meter.js
+package.json
+tmi.min.js 
+
+## Known Issues
+
+
---
+++ emote_meter.js
@@ -0,0 +1,119 @@
+//TODO: Check that only mods can enter !boom
+//TODO:  Why are some emotes showing before !boom
+//TODO:  Reconstruct the math so that there is a percentage
+//TODO:  Find out what the upper limit is for the total emotes
+//TODO: 
+//TODO:  Disappear the meter - find out when
+
+const gaugeElement = document.querySelector(".gauge");
+
+//const tmi = require('tmi.js');
+
+//var starting_value = 0;
+var value = 0;
+var totalEmotes = 0;
+var trigger = 0;
+
+function setGaugeValue(gauge, value) {
+  if (value < 0 || value > 1.01) {
+    console.log("starting value error");
+    return;
+  }
+
+ totalEmotes = totalEmotes + value;
+ if (totalEmotes >= .1) {
+	 trigger = 1;
+ }
+
+
+  console.log ("Total Emotes ",totalEmotes);
+  console.log ("Value ", value);
+
+  gauge.querySelector(".gauge__fill").style.transform = `rotate(${value / 2}turn)`;
+  gauge.querySelector(".gauge__cover").textContent = `${Math.round(totalEmotes * 100)}%`;
+ // gauge.querySelector(".gauge__cover").textContent = `${Math.round(value * 100)}%`;
+};
+
+      // ------------- for testing only 
+const myForm = document.getElementById("myForm");
+
+
+myForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        value = value + .005;
+        setGaugeValue(gaugeElement, value);
+
+//console.log("Counter has been incremented by .01");
+
+});
+      // ------------- for testing only 
+
+setGaugeValue(gaugeElement, value);
+
+const client = new tmi.Client({
+	options: { debug: true, messagesLogLevel: "info" },
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: '<username>',
+		password: '<oauth:token>'
+	},
+	channels: [ '<#channelname>' ]
+});
+client.connect().catch(console.error);
+client.on('message', (channel, tags, message, self) => {
+	if(self) return;
+	if(message.toLowerCase() === '!hello') {
+
+        //client.say(channel, `!boom FailFish`);
+  
+		//client.say(channel, `@${tags.username}, heya!`);
+
+	}
+	if(message.toLowerCase() === '!resetmeter') {
+		value = 0;
+		totalEmotes = 0;
+		trigger = 0;
+		return;
+	};
+
+
+	let all = [];
+	for (const key of Object.keys(tags.emotes)) {
+		const emote = tags.emotes[key];
+
+		for (const range of emote) {
+			const split = range.split('-');
+
+			all.push({
+				emote: key,
+				start: parseInt(split[0]),
+				end: parseInt(split[1]),
+			});
+		}
+	}
+	//console.log(all);
+	//console.log(all.length);
+	//totalEmotes = (totalEmotes + all.length) * .01;
+	//console.log(totalEmotes);
+	length = all.length * .01
+	setGaugeValue(gaugeElement, length);
+	if (trigger){
+		client.say(channel, `!boom`);
+		// **** Reset everything ****
+		value = 0;
+		totalEmotes = 0;
+		trigger = 0;
+	}
+
+  
+
+});
+
+
+
+
+
