60fb96d35609d7330930954125840799b5429d9a
astro <onetimefixation@gmail.com>
2021-12-21 18:13:17 -0500

    before timing is added

--- emote_meter.js
+++ emote_meter.js
@@ -1,16 +1,29 @@
 //TODO: Check that only mods can enter !boom
-//TODO:  Why are some emotes showing before !boom
+//TODO:  
 //TODO:  
 //TODO:  Find out what the upper limit is for the total emotes
 //TODO: 
 //TODO:  Disappear the meter - find out when
+//TODO:
+//TODO:• Can we have the meter shake more as it's filling towards 100%
+//TODO:• *******  Phase I - Start shaking at 80%
+//TODO:
+//TODO:• Explode when reaches full 100%, where the emotes leave the center of the emotes.
+//TODO:• *******  Do that in Stream Elements
+//TODO:
+//TODO:• The meter should lose gain when nothing is happening or the excitement isn't happening.
+//TODO:• *******  Idea - Tickle Me with Emotes" after x mins of inactivity
+//TODO:
+//TODO:• Can we have the emotes that pop over screen overlay, as the emotes used in the stream itself?
+//TODO:
 
 const gaugeElement = document.querySelector(".gauge");
+const gauge_shakingElement = document.querySelector(".gaugeshake");
 
 gaugeElement.querySelector(".gauge__fill");
 gaugeElement.querySelector(".gauge__cover").textContent = `0%`
-
-console.log ("At the top");
+gauge_shakingElement.querySelector(".gauge__fill");
+gauge_shakingElement.querySelector(".gauge__cover").textContent = `0%`
 
 //import dotenv from 'dotenv';
 
@@ -20,7 +33,7 @@ var totalPercentage = 0;
 var maxPercentage = 100;
 var trigger = 0;
 
-function setGaugeValue(gauge, value) {
+function setGaugeValue(gauge, gaugeshake, value) {
 
  totalEmotes = totalEmotes + value;
  totalPercentage = totalEmotes / maxPercentage;
@@ -33,9 +46,14 @@ function setGaugeValue(gauge, value) {
   console.log ("Value ", value);
   console.log ("Total Emote % ", (totalEmotes * .01) / 2);
 
-  gauge.querySelector(".gauge__fill").style.transform = `rotate(${(totalEmotes * .01) / 2}turn)`;
+
+
+ // gauge.querySelector(".gauge__fill").style.transform = `rotate(${(totalEmotes * .01) / 2}turn)`;
+  gauge.querySelector(".gauge__fill").style.transform = `rotate(${(totalEmotes) * (1.8)}deg)`;
   gauge.querySelector(".gauge__cover").textContent = `${Math.round(totalPercentage * 100)}%`;
 
+  gaugeshake.querySelector(".gauge__fill").style.transform = `rotate(${(totalEmotes) * (1.8)}deg)`;
+  gaugeshake.querySelector(".gauge__cover").textContent = `${Math.round(totalPercentage * 100)}%`;
 
 };
 
@@ -80,7 +98,25 @@ client.on('message', (channel, tags, message, self) => {
 //	length = all.length * .01;
 	length = all.length;
 	
-	setGaugeValue(gaugeElement, length);
+	setGaugeValue(gaugeElement, gauge_shakingElement, length);
+
+//	Timing
+//	======
+//	query the current Time
+var d = new Date(); // for now
+d.getHours(); // => 9
+d.getMinutes(); // =>  30
+d.getSeconds(); // => 5
+
+console.log(d);
+
+//	set timeout for currnt time + idle time (say 5 Mins)
+//	   if time expires 
+//		      say "tickle me"
+//			  then wait a minute, then dissapear the meter
+//	     
+//		 else 
+//		   <the idle timer should then be reset in setGaugeValue when new emote comes in
 	
 	if (trigger){
 		client.say(channel, `!boom`);        
