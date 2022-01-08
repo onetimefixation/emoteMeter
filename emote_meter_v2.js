d80189854b45557235207f3c01632bcd81122baf
astro <onetimefixation@gmail.com>
2022-01-08 11:20:43 -0500

    Last one with fade out

--- emote_meter_v2.js
+++ emote_meter_v2.js
@@ -3,7 +3,7 @@
 //TODO:  
 //TODO:  Find out what the upper limit is for the total emotes
 //TODO: 
-//TODO:  Disappear the meter - find out when
+//TODO:  Disappear the double first meters
 //TODO:
 //TODO:• Can we have the meter shake more as it's filling towards 100%
 //TODO:• *******  Phase I - Start shaking at 80%
@@ -29,83 +29,90 @@ gauge_shakingElement.querySelector(".gauge__cover").textContent = `0%`;
 
 //import dotenv from 'dotenv';
 
-var value = 0;
-var totalEmotes = 0;
-var totalPercentage = 0;
-var maxPercentage = 100;
-var trigger = 0;
+// ********************  DO NOT CHANGE THESE ******************** //
+var totalEmotes = 0;                                              //
+var totalPercentage = 0;                                          //
+var trigger = 0;                                                  //
+var stopped = 0;                                                  //
+var currTime = new Date();                                        //
+var currMinute = currTime.getMinutes();                           //
+var previousMinute = currMinute;                                  //
+var nextTime = currTime.getMinutes() + 5; // make 5 a param       //
+var timeoutID;                                                    //
+var firstTime = 1;                                                //
+var guageIsFadedOut = 0;                                          //         
+// ************************************************************** //
 
-var currTime = new Date();
-var currMinute = currTime.getMinutes();
-var previousMinute = currMinute;
-var delayTime = 10000; // 10 secs
-var nextTime = currTime.getMinutes() + 5; // make 5 a param
-var timeoutID;
-var firstTime = 1;
-var guageIsFadedOut = 0;
+
+
+// ***********************  THESE ARE CUSTOMIZABLE ************************* //
+var startToWiggle = 45;    // number of emotes                               //
+var maxPercentage = 100;   // not working yet                                //
+var delayTime = 10000;     // 1 sec = 1000 , 5 sec = 5000 , 30 secs = 30000  //
+// ************************************************************************* //
+
 
 function setGaugeValue(gauge, gaugeshake, value) {
 
  totalEmotes = totalEmotes + value;
  totalPercentage = totalEmotes / maxPercentage;
- if (totalPercentage *100 >= maxPercentage) {
-	 trigger = 1;
+
+ //if (totalPercentage *100 >= maxPercentage) {
+//	 trigger = 1;
+
+	 if (totalEmotes >= 50) {
+		trigger = 1;
+
  }
 
+ 
   console.log ("Total Percetage ",totalPercentage);
   console.log ("Total Emotes ",totalEmotes);
   console.log ("Value ", value);
   console.log ("Total Emote % ", (totalEmotes * .01) / 2);
 
 
-
- // gauge.querySelector(".gauge__fill").style.transform = `rotate(${(totalEmotes * .01) / 2}turn)`;
-  //gauge.querySelector(".gauge__fill").style.transform = `rotate(${(totalEmotes) * (1.8)}deg)`;
-  //gauge.querySelector(".gauge__cover").textContent = `${Math.round(totalPercentage * 100)}%`;
-
-  //gaugeshake.querySelector(".gauge__fill").style.transform = `rotate(${(totalEmotes) * (1.8)}deg)`;
-  //gaugeshake.querySelector(".gauge__cover").textContent = `${Math.round(totalPercentage * 100)}%`;
-
-
-// check if the shaking count has been reached.
-// if so swith the visibility of the two meters
-//
-
-if (totalEmotes >= 10) {
-
-// This needs to change because opacity still takes up space.
-
-	//gaugeElement.style.opacity = 0; 
-console.log("I am here")
-console.log("total Emotes in here: ", totalEmotes);
-// *********************************************************
-
-document.getElementById("showGauge").style.display = "none";
-document.getElementById("showShake").style.display = "";
-
-gaugeshake.querySelector(".gauge__fill").style.transform = `rotate(${(totalEmotes) * (1.8)}deg)`;
-gaugeshake.querySelector(".gauge__cover").textContent = `${Math.round(totalPercentage * 100)}%`;
-
-}
-
-else {
- // gauge.querySelector(".gauge__fill").style.transform = `rotate(${(totalEmotes * .01) / 2}turn)`;
+if (totalEmotes < startToWiggle) {
+	// check if the shaking count has been reached.
+    // if so swith the visibility of the two meters
+    // gauge.querySelector(".gauge__fill").style.transform = `rotate(${(totalEmotes * .01) / 2}turn)`;
  gauge.querySelector(".gauge__fill").style.transform = `rotate(${(totalEmotes) * (1.8)}deg)`;
  gauge.querySelector(".gauge__cover").textContent = `${Math.round(totalPercentage * 100)}%`;
+
 
  gaugeshake.querySelector(".gauge__fill").style.transform = `rotate(${(totalEmotes) * (1.8)}deg)`;
  gaugeshake.querySelector(".gauge__cover").textContent = `${Math.round(totalPercentage * 100)}%`;
  document.getElementById("showShake").style.display = "none";
 
 }
+else {
 
-};
+// **************  there is an issue here in that the shaky does not resiplay after it fades out and supposed
+// **************  to come back	
+// **************  Its not because of te display: none in CSS
+	document.getElementById("showGauge").style.display = "none";
+	gaugeshake.querySelector(".gauge__fill").style.transform = `rotate(${(totalEmotes) * (1.8)}deg)`;
+	gaugeshake.querySelector(".gauge__cover").textContent = `${Math.round(totalPercentage * 100)}%`;
+	document.getElementById("showShake").style.display = "inline-block";
+	}
 
+
+}
 //*********************  Goes Here ****************/
+
+
+
+
+//*********************  Ends Here ****************/
 
 client.connect().catch(console.error);
 client.on('message', (channel, tags, message, self) => {
 	if(self) return;
+
+	if (stopped) {
+		
+		return;
+	}
 
 	if(message.toLowerCase() === '!resetmeter') {
 		value = 0;
@@ -153,8 +160,21 @@ function resetTimer(nextTime){
 //  1)clear the last setTimeout 
 //  2) reset the nextTime and send this new nextTime to the resettimer so it starts a new clock
 
-	function fadeOutEffect() {
+function fadeOutEffect() {
+
+
+if (totalEmotes < startToWiggle) {	
+	// set a timeoout pulsingTimput, for x secs.
+
+
+
+	var pulseID = setTimeout (() => {
+          	// set the .pulse class
 	
+			document.getElementById("showGauge").classList.add("pulse");
+	}, 1000);
+
+
 		var fadeEffect = setInterval(function () {
 			if (!gaugeElement.style.opacity) {  
 				gaugeElement.style.opacity = 1;  
@@ -164,28 +184,52 @@ function resetTimer(nextTime){
 			} else {
 				clearInterval(fadeEffect);
 				guageIsFadedOut = 1;
+				console.log("in 3rd section")
 			}
-		}, 200);
+		}, 500);
 	}	
+else {
+	var fadeEffect = setInterval(function () {
 
+		if (!gauge_shakingElement.style.opacity) {  
+			gauge_shakingElement.style.opacity = 1;  
+		}
+		if (gauge_shakingElement.style.opacity > 0) {  
+			gauge_shakingElement.style.opacity -= 0.1; 
+		} else {
+			clearInterval(fadeEffect);
+			guageIsFadedOut = 1;
+		}
+	}, 200);
 
+}
+
+}
 
 	if (firstTime){
 		firstTime = 0;
-	}
+		}
 	else {  // clear the last timer and set a new one below
 		clearTimeout(timeoutID);
-		if (guageIsFadedOut){
-			//fade it back in ***************************************************
-			gaugeElement.style.opacity = 1;  
 
-			
-		}  // *******************************************************************
+
+
+		if (guageIsFadedOut){
+			//fade it back in *********************
+			gaugeElement.style.opacity = 1;  
+			gauge_shakingElement.style.opacity = 1
+		}  // *************************************
 	}
 
-	
+	//start the pulse effect
+
+
+
+
+
 	timeoutID = setTimeout(() => {
 			console.log("I'm going to sleep")
+
 			fadeOutEffect();
 		}, delayTime); //10 sec for now
 				
@@ -200,10 +244,10 @@ function resetTimer(nextTime){
 	if (trigger){
 		client.say(channel, `!boom`);        
 
-		// **** Reset everything ****
-		value = 0;
+		// **** Reset everything and quit ****
+		gauge_shakingElement.style.opacity = 0;
 		totalEmotes = 0;
-		trigger = 0;
+		stopped = 1;
 	}
 
 });
